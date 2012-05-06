eacaa4a147d7148308eda84f9e49db687a719962
Dmitry Bochkarev <dimabochkarev@gmail.com>
2012-05-06 21:37:47 +0600

    parameters asking

--- batmanjs/conf.js
+++ batmanjs/conf.js
@@ -2,21 +2,35 @@ var app = "<%=: app | downcase | underscore  %>";
 var controller = "<%=: controller | downcase | underscore  %>";
 var model = "<%=: model | downcase | underscore  %>";
 
-module.exports = {
+var t = module.exports = {
   templates: {
     app: {
+      params: {
+        app: "application name"
+      },
       file: "app.coffee",
       output: app + ".coffee"
     },
     controller: {
+      params: {
+        contoller: "controller name"
+      },
       file: "controller.coffee",
       output: controller + "_controller.coffee"
     },
     model: {
+      params: {
+        model: "model name"
+      },
       file: "model.coffee",
       output: model + ".coffee"
     },
     sample: {
+      params: {
+        app: "application name",
+        controller: "controller name",
+        model: "model name"
+      },
       dirs: [app + "/controllers", app + "/models"],
       files: [
         ["app.coffee", app + "/" + app + ".coffee"],
